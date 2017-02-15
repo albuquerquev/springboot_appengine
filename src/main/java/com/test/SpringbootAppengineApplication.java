8a8b13f7a9e34baf2e5bd7a44ce4c7f60381c518
Vijay Albuquerque <misc@techvj.com>
2017-02-15 16:00:44 +0530

    Added spring-boot-starter-web And A REST Endpoint.

--- src/main/java/com/test/SpringbootAppengineApplication.java
+++ src/main/java/com/test/SpringbootAppengineApplication.java
@@ -2,11 +2,18 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootAppengineApplication {
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Spring Boot + App Engine!";
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAppengineApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAppengineApplication.class, args);
+  }
 }
