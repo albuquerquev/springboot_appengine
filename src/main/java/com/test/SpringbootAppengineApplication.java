378e03c305c699e73f5a69ea30a0749b5ad0328b
Vijay Albuquerque <misc@techvj.com>
2017-02-15 11:44:50 +0530

    Initial Commit - Empty Spring Boot Project.

---
+++ src/main/java/com/test/SpringbootAppengineApplication.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppengineApplication.class, args);
+	}
+}
